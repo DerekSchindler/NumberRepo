220cd699ca98b4023c715b313154f6237921b967
Derek S <derek.schindler.2022@studet.lhchools.org>
2019-03-08 07:48:39 -0500

    Initialized random number and scanner class

---
+++ src/NumberGuessing.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class NumberGuessing {
+    public static void main(String[]args){
+
+        int number = (int)(Math.random()*100);
+        Scanner anthem;
+        anthem = new Scanner(System.in);
+
+        
+
+
+
+
+
+
+    }
+
+
+}
