3567220ded161c78ea3509c69738d8a6b1ac1f6d
Derek S <derek.schindler.2022@studet.lhchools.org>
2019-03-14 07:45:35 -0400

    Implemented Loop and completed project

--- src/NumberGuessing.java
+++ src/NumberGuessing.java
@@ -6,8 +6,27 @@ public class NumberGuessing {
         int number = (int)(Math.random()*100);
         Scanner anthem;
         anthem = new Scanner(System.in);
+        int guess;
 
-        
+        System.out.println("Guess a number between 1-100");
+        guess = anthem.nextInt();
+
+        while(guess!=number){
+            if(guess>number){
+                System.out.println("Your guess is too high. Please ty again.");
+                guess = anthem.nextInt();
+            }
+           else if(guess<number){
+                System.out.println("Your guess is too low. Please try again.");
+                guess = anthem.nextInt();
+            }
+            }
+
+
+        if(guess==number){
+            System.out.println("You guessed the right number.");
+        }
+
 
 
 
@@ -15,6 +34,4 @@ public class NumberGuessing {
 
 
     }
-
-
 }
