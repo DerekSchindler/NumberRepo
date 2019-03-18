5a32b2aa1a2de3355c9724c71200627a5623bed9
Derek S <derek.schindler.2022@studet.lhchools.org>
2019-03-18 07:35:07 -0400

    Added a set number of guesses

--- src/NumberGuessing.java
+++ src/NumberGuessing.java
@@ -7,24 +7,31 @@ public class NumberGuessing {
         Scanner anthem;
         anthem = new Scanner(System.in);
         int guess;
+        int attempt = 10;
 
         System.out.println("Guess a number between 1-100");
         guess = anthem.nextInt();
 
-        while(guess!=number){
+        while(guess!=number&&attempt!=0){
             if(guess>number){
                 System.out.println("Your guess is too high. Please ty again.");
-                guess = anthem.nextInt();
+
             }
            else if(guess<number){
                 System.out.println("Your guess is too low. Please try again.");
-                guess = anthem.nextInt();
+
             }
+            System.out.println("You have "+attempt+" attempt left");
+            guess = anthem.nextInt();
+            attempt=attempt-1;
             }
 
 
         if(guess==number){
             System.out.println("You guessed the right number.");
+        }
+        else{
+            System.out.println("You ran out of attempts.");
         }
 
 
